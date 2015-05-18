7259bc2372333758d702f55954f3f7f30afc75b2
Marco Miglierina <marco.miglierina@icloud.com>
2015-05-18 11:20:52 +0200

    added missing resource for testing and removed useless test on architectural constraints

--- rules/src/test/java/it/polimi/tower4clouds/rules/Constraint2RuleTest.java
+++ rules/src/test/java/it/polimi/tower4clouds/rules/Constraint2RuleTest.java
@@ -51,30 +51,6 @@ public class Constraint2RuleTest {
 			fail();
 		}
 	}
-
-	@Test
-	public void rulesFromArchitecturalConstraintsShouldNotValidate() {
-		try {
-			InputStream testRulesStream = getClass().getResourceAsStream(
-					"/architecturalConstraints.xml");
-			Constraints constraints = XMLHelper.deserialize(testRulesStream,
-					Constraints.class);
-			MonitoringRuleFactory factory = new MonitoringRuleFactory();
-			MonitoringRules rules = factory
-					.makeRulesFromQoSConstraints(constraints);
-			RulesValidator validator = new RulesValidator();
-			Set<Problem> problems = validator.validateAllRules(rules);
-
-			for (Problem problem : problems) {
-				System.out.println(problem.toString());
-			}
-			
-			assertFalse(problems.isEmpty());
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail();
-		}
-	}
 	
 	@Test
 	public void randomConstraintsShouldBeTranslatedButNotValidated() {
