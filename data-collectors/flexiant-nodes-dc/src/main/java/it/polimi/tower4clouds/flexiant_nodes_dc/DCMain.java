7b91d6e56a6cae3225f95557537a909e009a6d1c
Davide Verzotto <davide.verzotto@mail.polimi.it>
2015-08-24 12:54:43 +0200

    Made the DC executable

---
+++ data-collectors/flexiant-nodes-dc/src/main/java/it/polimi/tower4clouds/flexiant_nodes_dc/DCMain.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2015 Politecnico di Milano.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package it.polimi.tower4clouds.flexiant_nodes_dc;
+
+import it.polimi.modaclouds.qos_models.util.XMLHelper;
+import it.polimi.tower4clouds.manager.api.ManagerAPI;
+import it.polimi.tower4clouds.rules.MonitoringRules;
+import java.util.Properties;
+
+/**
+ *
+ * @author davide
+ */
+public class DCMain {
+    
+    private static final String graphiteIP = "localhost";
+    private static final int graphitePort = 8001;
+    private static String managerIP = "localhost";
+    private static int managerPort = 8170;
+    
+    public static void main(String[] args) throws Exception {
+        
+        Properties flexDCProp = new Properties();
+        flexDCProp.put(DCProperty.URL_NODES_FILE1, "https://cp.sd1.flexiant.net/nodeid/Cluster1.csv");
+        flexDCProp.put(DCProperty.URL_NODES_FILE2, "https://cp.sd1.flexiant.net/nodeid/Cluster2.csv");
+        flexDCProp.put(DCProperty.URL_CPU_METRIC, "https://cp.sd1.flexiant.net/nodecpu10/");
+        flexDCProp.put(DCProperty.URL_RAM_METRIC, "https://cp.sd1.flexiant.net/noderam/");
+        flexDCProp.put(DCProperty.URL_NODELOAD_METRIC, "https://cp.sd1.flexiant.net/nodeload/");
+        flexDCProp.put(DCProperty.URL_TXNETWORK_METRIC, "https://cp.sd1.flexiant.net/nodenet/");
+        flexDCProp.put(DCProperty.URL_RXNETWORK_METRIC, "https://cp.sd1.flexiant.net/nodenet/");
+        flexDCProp.put(DCProperty.URL_STORAGE_METRIC, "https://cp.sd1.flexiant.net/storage/");
+        
+	ManagerAPI manager = new ManagerAPI(managerIP, managerPort);
+        
+	manager.installRules(XMLHelper.deserialize(DCMain.class
+			.getResourceAsStream("/rules.xml"),
+			MonitoringRules.class));
+            
+        manager.registerHttpObserver("CpuUtilization", "http://" + graphiteIP + ":" + graphitePort + "/data", "GRAPHITE");
+        manager.registerHttpObserver("RamUtilization", "http://" + graphiteIP + ":" + graphitePort + "/data", "GRAPHITE");
+        manager.registerHttpObserver("NodeLoad", "http://" + graphiteIP + ":" + graphitePort + "/data", "GRAPHITE");
+        manager.registerHttpObserver("TXNetwork", "http://" + graphiteIP + ":" + graphitePort + "/data", "GRAPHITE");
+        manager.registerHttpObserver("RXNetwork", "http://" + graphiteIP + ":" + graphitePort + "/data", "GRAPHITE");
+        manager.registerHttpObserver("StorageMetric", "http://" + graphiteIP + ":" + graphitePort + "/data", "GRAPHITE");
+        Registry.initialize(managerIP, managerPort, flexDCProp);
+        Registry.startMonitoring();
+        
+        Thread.sleep(5 * 60 * 1000);
+        //Close the application after 5 minutes
+        Registry.stopMonitoring();
+        System.exit(0);
+        
+    }
+    
+}
